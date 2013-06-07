c3fba406fa4c7afeb941a59009b2441a05cf5e38
Alexander Nikitin <nag@albatrosscom.ru>
2013-06-07 17:26:29 +0700

    Fix import from http://rambler.ru

--- plugins/rambler.plg.php
+++ plugins/rambler.plg.php
@@ -1,35 +1,39 @@
 <?php
-$_pluginInfo=array(
-	'name'=>'Rambler',
-	'version'=>'1.1.7',
-	'description'=>"Get the contacts from a Rambler account",
-	'base_version'=>'1.8.3',
-	'type'=>'email',
-	'check_url'=>'http://www.rambler.ru',
-	'requirement'=>'email',
-	'allowed_domains'=>array('/(rambler.ru)/i'),
-	'imported_details'=>array('first_name','email_1'),
-	);
+$_pluginInfo = array(
+	'name'             => 'Rambler',
+	'version'          => '1.2.0',
+	'description'      => "Get the contacts from a Rambler account",
+	'base_version'     => '1.8.3',
+	'type'             => 'email',
+	'check_url'        => 'http://www.rambler.ru',
+	'requirement'      => 'user',
+	'allowed_domains'  => array('/(rambler.ru)/i'),
+	'imported_details' => array(
+    'first_name',
+    'email_1'
+  ),
+);
+
 /**
  * Rambler Plugin
  * 
  * Import user's contacts from Rambler AddressBook
  * 
- * @author OpenInviter
- * @version 1.0.9
+ * @author BarBQ
+ * @version 1.2.0
  */
 class rambler extends openinviter_base
-	{
-	private $login_ok=false;
-	public $showContacts=true;
-	public $internalError=false;
-	protected $timeout=30;
+{
+	private   $login_ok      = false;
+	public    $showContacts  = true;
+	public    $internalError = false;
+	protected $timeout       = 30;
 	
-	public $debug_array=array(
-				'initial_get'=>'login',
-				'login_post'=>'ramac_add_handler',
-				'url_contacts'=>'email'
-				);
+	public    $debug_array   = array(
+    'initial_get'  => 'login',
+    'login_post'   => 'ramac_add_handler',
+    'url_contacts' => 'email'
+	);
 	
 	/**
 	 * Login function
@@ -42,44 +46,44 @@ class rambler extends openinviter_base
 	 * @return bool TRUE if the current user was authenticated successfully, FALSE otherwise.
 	 */
 	public function login($user,$pass)
-		{
+	{
 		$this->resetDebugger();
-		$this->service='rambler';
-		$this->service_user=$user;
-		$this->service_password=$pass;
-		if (!$this->init()) return false;
+		$this->service          = 'rambler';
+		$this->service_user     = $user;
+		$this->service_password = $pass;
+
+		if (!$this->init()) {
+      return false;
+    }
 				
 		$res=$this->get("http://www.rambler.ru/",true);
 		
-		if ($this->checkResponse("initial_get",$res))
-			$this->updateDebugBuffer('initial_get',"http://www.rambler.ru/",'GET');
-		else
-			{
+		if ($this->checkResponse("initial_get",$res)) {
+      $this->updateDebugBuffer('initial_get',"http://www.rambler.ru/",'GET');
+    }	else	{
 			$this->updateDebugBuffer('initial_get',"http://www.rambler.ru/",'GET',false);
 			$this->debugRequest();
 			$this->stopPlugin();
+
 			return false;
-			}
-		$post_elements=$this->getHiddenElements($res);$post_elements['login']=$user;$post_elements['passw']=$pass; 
-		unset($post_elements[0]); 
-		$res=$this->post("http://id.rambler.ru/script/auth.cgi",$post_elements,true);
-		if ($this->checkResponse("login_post",$res))
-			$this->updateDebugBuffer('login_post',"http://id.rambler.ru/script/auth.cgi",'POST',true,$post_elements);
-		else
-			{
-			$this->updateDebugBuffer('login_post',"http://id.rambler.ru/script/auth.cgi",'POST',false,$post_elements);
-			$this->debugRequest();
-			$this->stopPlugin();
-			return false;
-			}
-		
-		$url_contact_array=$this->getElementDOM($res,"//a[@id='addressbook-link']",'href');
-		$value=substr($url_contact_array[0],strpos($url_contact_array[0],"r=")+2,strlen($url_contact_array[0])-strpos($url_contact_array[0],"r=")-2);
-		$url_contact="http://mail.rambler.ru/mail/contacts.cgi?r={$value}";
-		$this->login_ok=$url_contact;
-		file_put_contents($this->getLogoutPath(),$value);
-		return true;
 		}
+
+		$post_elements                     = $this->getHiddenElements($res);
+		$post_elements['profile.login']    = $user;
+		$post_elements['profile.password'] = $pass;
+		$post_elements['profile.domain']   = 'rambler.ru';
+		$post_elements['button.submit']    = '';
+		$post_elements['show']             = '';
+
+		unset($post_elements[0]);
+
+    $customHeaders  = array('User-Agent' => 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:20.0) Gecko/20100101 Firefox/20.0');
+    $res            = $this->post("http://id.rambler.ru/login", $post_elements, true, false, false, $customHeaders);
+
+		$this->login_ok = 'http://mail.rambler.ru/jsonrpc2';
+
+		return true;
+	}
 
 	/**
 	 * Get the current user's contacts
@@ -90,32 +94,53 @@ class rambler extends openinviter_base
 	 * @return mixed The array if contacts if importing was successful, FALSE otherwise.
 	 */	
 	public function getMyContacts()
-		{
-		if (!$this->login_ok)
-			{
+	{
+		if (!$this->login_ok) {
 			$this->debugRequest();
 			$this->stopPlugin();
+
 			return false;
-			}
-		else $url=$this->login_ok;
-		$res=$this->get($url,true);
-		if ($this->checkResponse("url_contacts",$res))
-			$this->updateDebugBuffer('url_contacts',$url,'GET');
-		else
-			{
-			$this->updateDebugBuffer('url_contacts',$url,'GET',false);
-			$this->debugRequest();
-			$this->stopPlugin();
-			return false;
-			}
-		$contacts=array();
-		$emailsArray=$this->getElementDOM($res,"//td[@class='mtbox-cell mtbox-email quiet']");
-		$namesArray=$this->getElementDOM($res,"//a[@class='mtbox-action mtbox-view']");		
-		if (!empty($emailsArray))
-			foreach($emailsArray as $key=>$emailValue) $contacts[trim($emailValue)]=array('first_name'=>(isset($namesArray[$key])?trim($namesArray[$key]):false),'email_1'=>trim($emailValue));			
-		foreach ($contacts as $email=>$name) if (!$this->isEmail($email)) unset($contacts[$email]);
-		return $this->returnContacts($contacts);
 		}
+
+    $jsonData = '{"method":"Rambler::Mail::get_contacts","jsonrpc":"2.0","id":"136617668530575907","params":{}}';
+
+    $customHeaders = array(
+      'Content-Type'     => 'application/json; charset=UTF-8',
+      'X-Requested-With' => 'XMLHttpRequest',
+      'User-Agent'       => 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:20.0) Gecko/20100101 Firefox/20.0',
+      'Accept: '         => 'application/json, text/javascript, */*; q=0.01',
+      'Referer'          => 'http://mail.rambler.ru/',
+     );
+
+    $res = $this->post($this->login_ok, $jsonData, false, false, false, $customHeaders, true, false);
+
+    if ($res) {
+      $response = json_decode($res, true);
+
+      if ($response['result']['status'] != 'OK') {
+        return false;
+      }
+
+      $contactsData = $response['result']['addressbook']['contacts'];
+    }
+
+    $contacts = array();
+
+    foreach ($contactsData as $contact) {
+      $email = trim($contact['email']);
+
+      if ($email) {
+        $firstName        = $contact['first_name'];
+        $lastName         = $contact['last_name'];
+        $contacts[$email] = array(
+          'first_name' => $firstName,
+          'last_name'  => $lastName
+        );
+      }
+    }
+
+		return $this->returnContacts($contacts);
+	}
 
 	/**
 	 * Terminate session
@@ -127,17 +152,17 @@ class rambler extends openinviter_base
 	 * @return bool TRUE if the session was terminated successfully, FALSE otherwise.
 	 */	
 	public function logout()
-		{
-		if (!$this->checkSession()) return false;
-		if (file_exists($this->getLogoutPath()))
-			{
-			$url_logout="http://id.rambler.ru/script/auth.cgi?back=;mode=logout;r=".file_get_contents($this->getLogoutPath());
-			$res=$this->get($url_logout,true);
-			}
-		$this->debugRequest();			
+	{
+		if (!$this->checkSession()) {
+      return false;
+    }
+
+    $this->get('http://id.rambler.ru/logout?back=http://rambler.ru/&rname=mail',true);
+
+    $this->debugRequest();
 		$this->resetDebugger();
 		$this->stopPlugin();
+
 		return true;
-		}
-	}	
-?>
+	}
+}
